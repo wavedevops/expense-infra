608b2b519d328d2f988ca43159ebceafe7661ab7
Chowdary-Hari <chandrahari321@outlook.com>
2024-10-09 11:28:15 +0530

    automated commit #151

--- terraform/02-sg/extra-vpc.tf
+++ terraform/02-sg/extra-vpc.tf
@@ -1,11 +1,33 @@
+
+# #added as part of Jenkins CICD
+# resource "aws_security_group_rule" "backend_default_vpc" {
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   cidr_blocks = ["172.31.0.0/16"]
+#   security_group_id = module.backend.sg_id
+# }
+#
+# #added as part of Jenkins CICD
+# resource "aws_security_group_rule" "frontend_default_vpc" {
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   cidr_blocks = ["172.31.0.0/16"]
+#   security_group_id = module.frontend.sg_id
+# }
+
 ## db
+
 resource "aws_security_group_rule" "db_allow_all" {
   description       = "Allow SSH access from allow all security group"
   type              = "ingress"
   from_port         = 3306
   to_port           = 3306
   protocol          = "tcp"
-  source_security_group_id = data.aws_security_group.allow_all.id  # Use default security group ID
+  cidr_blocks = ["172.31.0.0/16"]  # Use default security group ID
   security_group_id = module.database.sg_id  # Reference to your security group ID
 }
 
@@ -16,7 +38,7 @@ resource "aws_security_group_rule" "backend_ssh_allow_all" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  source_security_group_id = data.aws_security_group.allow_all.id  # Use default security group ID
+  cidr_blocks = ["172.31.0.0/16"]  # Use default security group ID
   security_group_id = module.backend.sg_id  # Reference to your security group ID
 }
 
@@ -26,7 +48,7 @@ resource "aws_security_group_rule" "backend_http_allow_all" {
   from_port         = 8080
   to_port           = 8080
   protocol          = "tcp"
-  source_security_group_id = data.aws_security_group.allow_all.id  # Use default security group ID
+  cidr_blocks = ["172.31.0.0/16"]  # Use default security group ID
   security_group_id = module.backend.sg_id  # Reference to your security group ID
 }
 
@@ -37,7 +59,7 @@ resource "aws_security_group_rule" "frontend_ssh_allow_all" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  source_security_group_id = data.aws_security_group.allow_all.id  # Use default security group ID
+  cidr_blocks = ["172.31.0.0/16"]  # Use default security group ID
   security_group_id = module.frontend.sg_id  # Reference to your security group ID
 }
 
@@ -47,7 +69,7 @@ resource "aws_security_group_rule" "frontend_http_allow_all" {
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  source_security_group_id = data.aws_security_group.allow_all.id  # Use default security group ID
+  cidr_blocks = ["172.31.0.0/16"]  # Use default security group ID
   security_group_id = module.frontend.sg_id  # Reference to your security group ID
 }
 
