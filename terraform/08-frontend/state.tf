94efdfef9e44dc2b7cc69fed5b927e287ec845b6
Chowdary-Hari <chandrahari321@outlook.com>
2024-10-10 13:57:30 +0530

    automated commit #163

--- terraform/08-frontend/state.tf
+++ terraform/08-frontend/state.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "chowdary-hari"                 # S3 bucket name for storing Terraform state
-    key    = "test/backend/terraform.state"      # State file path inside the S3 bucket
+    key    = "test/frontend/terraform.state"      # State file path inside the S3 bucket
     region = "us-east-1"                     # AWS region where the S3 bucket is located
   }
 }
