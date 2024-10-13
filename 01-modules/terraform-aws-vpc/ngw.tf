e0f587343817f32007ffcb807bc38d7490f4795b
Chowdary-Hari <chandrahari321@outlook.com>
2024-10-13 17:59:10 +0530

    automated commit #222

--- 01-modules/terraform-aws-vpc/ngw.tf
+++ 01-modules/terraform-aws-vpc/ngw.tf
@@ -1,28 +1,28 @@
-# resource "aws_eip" "nat" {
-#   domain   = "vpc"
-# }
-#
-# resource "aws_nat_gateway" "nat" {
-#   allocation_id = aws_eip.nat.id
-#   subnet_id     = aws_subnet.public[0].id
-#
-#   tags = merge(
-#     var.common_tags,
-#     var.nat_gateway_tags,
-#     {
-#       Name = "${local.resource_name}" #expense-dev
-#     }
-#   )
-# }
-#
-# resource "aws_route" "private_route_nat" {
-#   route_table_id            = aws_route_table.private.id
-#   destination_cidr_block    = "0.0.0.0/0"
-#   nat_gateway_id = aws_nat_gateway.nat.id
-# }
-#
-# resource "aws_route" "database_route_nat" {
-#   route_table_id            = aws_route_table.database.id
-#   destination_cidr_block    = "0.0.0.0/0"
-#   nat_gateway_id = aws_nat_gateway.nat.id
-# }
+resource "aws_eip" "nat" {
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(
+    var.common_tags,
+    var.nat_gateway_tags,
+    {
+      Name = "${local.resource_name}" #expense-dev
+    }
+  )
+}
+
+resource "aws_route" "private_route_nat" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat.id
+}
+
+resource "aws_route" "database_route_nat" {
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat.id
+}
