cb4bb0a50818d2730a06b4fc264e4759e57e10d5
Chowdary-Hari <chandrahari321@outlook.com>
2024-10-18 19:40:44 +0530

    automated commit #270

--- 01-vpc/vpc.tf
+++ 01-vpc/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "./terraform-aws-vpc"
+    source = "terraform-aws-vpc"
     project_name = var.project_name
     common_tags = var.common_tags
     public_subnet_cidrs = var.public_subnet_cidrs
--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -1,5 +1,5 @@
 module "db" {
-  source = "./terraform-aws-securitygroup"
+  source = "terraform-aws-securitygroup"
   project_name = var.project_name
   environment = var.environment
   sg_description = "SG for DB MySQL Instances"
@@ -9,7 +9,7 @@ module "db" {
 }
 
 module "backend" {
-  source = "./terraform-aws-securitygroup"
+  source = "terraform-aws-securitygroup"
   project_name = var.project_name
   environment = var.environment
   sg_description = "SG for Backend Instances"
@@ -19,7 +19,7 @@ module "backend" {
 }
 
 module "app_alb" {
-  source = "./terraform-aws-securitygroup"
+  source = "terraform-aws-securitygroup"
   project_name = var.project_name
   environment = var.environment
   sg_description = "SG for APP ALB Instances"
@@ -30,7 +30,7 @@ module "app_alb" {
 
 
 module "frontend" {
-  source = "./terraform-aws-securitygroup"
+  source = "terraform-aws-securitygroup"
   project_name = var.project_name
   environment = var.environment
   sg_description = "SG for Frontend Instances"
@@ -40,7 +40,7 @@ module "frontend" {
 }
 
 module "web_alb" {
-  source = "./terraform-aws-securitygroup"
+  source = "terraform-aws-securitygroup"
   project_name = var.project_name
   environment = var.environment
   sg_description = "SG for Web ALB Instances"
@@ -50,7 +50,7 @@ module "web_alb" {
 }
 
 module "bastion" {
-  source = "./terraform-aws-securitygroup"
+  source = "terraform-aws-securitygroup"
   project_name = var.project_name
   environment = var.environment
   sg_description = "SG for Bastion Instances"
@@ -60,7 +60,7 @@ module "bastion" {
 }
 
 module "vpn" {
-  source = "./terraform-aws-securitygroup"
+  source = "terraform-aws-securitygroup"
   project_name = var.project_name
   environment = var.environment
   sg_description = "SG for VPN Instances"
